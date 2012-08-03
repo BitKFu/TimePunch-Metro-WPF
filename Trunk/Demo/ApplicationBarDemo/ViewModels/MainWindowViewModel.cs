75c846f150f61212fbe326ba728469cc0326260b
BitKFu_cp <SND\BitKFu_cp>
2012-08-03 21:08:25 +0000

    First Demo Application

---
+++ Trunk/Demo/ApplicationBarDemo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using TimePunch.Metro.Wpf.ViewModel;
+
+namespace ApplicationBarDemo.ViewModels
+{
+    /// <summary>
+    /// The MainWindow ViewModel used to handle messages for the MainWindow
+    /// </summary>
+    public class MainWindowViewModel : MainWindowViewModelBase
+    {
+    }
+}
