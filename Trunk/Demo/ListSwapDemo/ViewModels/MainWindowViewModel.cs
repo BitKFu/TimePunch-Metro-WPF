ddadab8c2dd7b7c68d660d57e3febf337881f13d
BitKFu_cp <SND\BitKFu_cp>
2012-08-13 20:03:26 +0000

    Implemented a demo for the ListSwap Scenario

---
+++ Trunk/Demo/ListSwapDemo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using TimePunch.Metro.Wpf.ViewModel;
+
+namespace ListSwapDemo.ViewModels
+{
+    /// <summary>
+    /// The MainWindow ViewModel used to handle messages for the MainWindow
+    /// </summary>
+    public class MainWindowViewModel : MainWindowViewModelBase
+    {
+    }
+}
